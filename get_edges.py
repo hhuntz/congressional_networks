36bbcc643c78f5e736bd9cf575f51a31cbd02c09
hhuntz <holdenh@umich.edu>
2022-12-02 23:22:00 -0500

    combine collect_sponsor_data w/ get_edges

--- get_edges.py
+++ get_edges.py
@@ -1,7 +1,54 @@
 import json
 import csv
+import os
 
-# nodes are legislator download from https://github.com/unitedstates/congress-legislators
+def get_sponsor_data():
+    '''
+    gets sponsor dict for ~324,000 total bills
+    iterates through data in 'data' folder from 
+    '''
+    sponsor_dict = {}
+    i = 0
+    errors = 0
+    for session in range(93, 117):
+        sponsor_dict[session] = {}
+
+        path = os.path.join(os.path.dirname(__file__), f'data/{str(session)}')
+
+        for bill_type in os.listdir(path):
+            if '.DS_Store' in bill_type:
+                continue
+            type_path = os.path.join(path, bill_type)
+            for bill in os.listdir(type_path):
+                bill_path = os.path.join(type_path, bill)
+                if '.DS_Store' in bill_path:
+                    continue
+                try:
+                    sponsor_dict[session][bill] = {}
+                    data_path = bill_path + '/data.json'
+                    with open(data_path, 'r') as f:
+                        j = json.loads(f.read())
+                        # get IDs for MOCs sponsoring the bill
+                        try:
+                            # older system was thomas id -- appending 't' at front
+                            sponsor_dict[session][bill]['sponsor'] = j['sponsor']['thomas_id']   
+                            sponsor_dict[session][bill]['cosponsors'] = [cosponsor['thomas_id'] for cosponsor in j['cosponsors']]
+                        except: 
+                            # newer system is bioguide id -- appending 'b' at front
+                            sponsor_dict[session][bill]['sponsor'] = j['sponsor']['bioguide_id']   
+                            sponsor_dict[session][bill]['cosponsors'] = [cosponsor['bioguide_id'] for cosponsor in j['cosponsors']]
+                    
+                    i += 1
+                    if i % 1000 == 0:
+                        print(f'{i} bills done') 
+
+                except:
+                    # errors seem to be bills without sponsor info (saved as 'null' in the json)
+                    # error rate is about 1.5 in 10,000
+                    errors += 1
+                    print(f'skipped {bill} from {session} ------ {errors} errors')
+                    
+    return sponsor_dict
 
 def get_edge_list(sponsors_dict):
     edge_dict = {}
@@ -29,19 +76,30 @@ def get_edge_list(sponsors_dict):
     print('done making edge list')
     return edge_dict
 
+def write_sponsors_json(d):
+    print('writing JSON file')
+    with open('data/sponsors.json', 'w+') as f:
+        dict_str = json.dumps(d)
+        f.write(dict_str)
+
 def write_edge_csv(lst):
     print('writing csv file...')
-    with open('data/edges.csv', 'w+') as f:
+    path = os.path.join(os.path.dirname(__file__), 'data/edges.csv')
+    with open(path, 'w+') as f:
         for line in lst:
             f.write(f"{line}\n")
     print('all done!')
 
 
 if __name__ == '__main__':
-    with open('data/sponsors.json') as f:
-        sponsors_json = f.read()
-    sponsors = json.loads(sponsors_json)
+    sponsors = get_sponsor_data()
+    # write_json(sponsors)
+
+    # with open('data/sponsors.json') as f:
+    #     sponsors_json = f.read()
+    # sponsors = json.loads(sponsors_json)
+
     edge_dict = get_edge_list(sponsors)
     edges = [f'{k},{v}' for k,v in edge_dict.items()]
-    print(edges)
+
     write_edge_csv(edges)
